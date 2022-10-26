21655294b19545347c071b6bcb4e3cf50f4552e7
Name <email@example.ru>
2022-10-26 19:03:57 +0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
+
+// npm init @eslint/config
+//     ./node_modules/.bin/eslint webpack.development.js
